0aa96605385f220d10841af6b81d52caf76e7b07
Jan Kiszka <jan.kiszka@siemens.com>
2018-01-09 16:47:57 +0100

    x86: Use per-cpu page tables
    This lays the ground for hiding truly private per-cpu data, though this
    step is left for later.
    
    CPU-local page tables allow to replace the per-cpu accessors with a
    simpler mechanism: just define a region that maps the local per-cpu
    structure at the same virtual address for each CPU.
    
    As this_cpu_* now returns pointers to the local per-cpu mapping region,
    we need to be careful to use it only as long as this mapping persists,
    ie. from arch_cpu_init until arch_cpu_restore.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/arch/x86/setup.c
+++ hypervisor/arch/x86/setup.c
@@ -148,9 +148,22 @@ int arch_cpu_init(struct per_cpu *cpu_data)
 	read_descriptor(cpu_data, &cpu_data->linux_gs);
 	cpu_data->linux_gs.base = read_msr(MSR_GS_BASE);
 
-	/* set up per-CPU helpers */
-	write_msr(MSR_GS_BASE, (unsigned long)cpu_data);
-	cpu_data->cpu_data = cpu_data;
+	/* set up per-CPU page table */
+	cpu_data->pg_structs.hv_paging = true;
+	cpu_data->pg_structs.root_paging = hv_paging_structs.root_paging;
+	cpu_data->pg_structs.root_table =
+		(page_table_t)cpu_data->root_table_page;
+
+	err = paging_create_hvpt_link(&cpu_data->pg_structs, JAILHOUSE_BASE);
+	if (err)
+		return err;
+
+	/* set up private mapping of per-CPU data structure */
+	err = paging_create(&cpu_data->pg_structs, paging_hvirt2phys(cpu_data),
+			    sizeof(*cpu_data), LOCAL_CPU_BASE,
+			    PAGE_DEFAULT_FLAGS, PAGING_NON_COHERENT);
+	if (err)
+		return err;
 
 	/* read registers to restore on first VM-entry */
 	for (n = 0; n < NUM_ENTRY_REGS; n++)
@@ -188,7 +201,7 @@ int arch_cpu_init(struct per_cpu *cpu_data)
 
 	/* swap CR3 */
 	cpu_data->linux_cr3 = read_cr3();
-	write_cr3(paging_hvirt2phys(hv_paging_structs.root_table));
+	write_cr3(paging_hvirt2phys(cpu_data->pg_structs.root_table));
 
 	cpu_data->pat = read_msr(MSR_IA32_PAT);
 	write_msr(MSR_IA32_PAT, PAT_RESET_VALUE);
