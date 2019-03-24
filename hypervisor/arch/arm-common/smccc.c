yshalabi/jailhouse

commit 075879bd0d480edec05e8f894d6b7af0ab0ccffb

arm-common: crash cell on unhandled SMC traps
Crash a cell if it calls an unhandled SMC functions.

There are two reason why to do this:
  - Not all SMC calls have return values. If the hypervisor returns with
    UNHANDLED, the guest may silently fail as it takes wrong
    assumptions. (This is what already happened to us)
  - A guest may only invoke SMC functions which it has discovered
    before. If there are new functions that we might have to implement
    in future, the crash will lead us the way.

Note that the default handler crashes the cell in case of
ARCH_SMCCC_WORKAROUND_1, as the default handler path will only be taken
in case of default interrupt vectors, where the workaround is not
available.

Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
[Jan: preserve function_id variable for better readability]
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/arm-common/smccc.c
@@ -62,32 +62,38 @@ static inline long handle_arch_features(u32 id)
 	}
 }
 
-static long handle_arch(struct trap_context *ctx)
+static enum trap_return handle_arch(struct trap_context *ctx)
 {
 	u32 function_id = ctx->regs[0];
+	unsigned long *ret = &ctx->regs[0];
 
 	switch (function_id) {
 	case SMCCC_VERSION:
-		return ARM_SMCCC_VERSION_1_1;
+		*ret = ARM_SMCCC_VERSION_1_1;
+		break;
 
 	case SMCCC_ARCH_FEATURES:
-		return handle_arch_features(ctx->regs[1]);
+		*ret = handle_arch_features(ctx->regs[1]);
+		break;
 
 	default:
-		return ARM_SMCCC_NOT_SUPPORTED;
+		panic_printk("Unhandled SMC arch trap %lx\n", *ret);
+		return TRAP_UNHANDLED;
 	}
+
+	return TRAP_HANDLED;
 }
 
 enum trap_return handle_smc(struct trap_context *ctx)
 {
 	unsigned long *regs = ctx->regs;
+	enum trap_return ret = TRAP_HANDLED;
 	u32 *stats = this_cpu_public()->stats;
 
 	switch (SMCCC_GET_OWNER(regs[0])) {
 	case ARM_SMCCC_OWNER_ARCH:
 		stats[JAILHOUSE_CPU_STAT_VMEXITS_SMCCC]++;
-		regs[0] = handle_arch(ctx);
-		break;
+		ret = handle_arch(ctx);
 
 	case ARM_SMCCC_OWNER_SIP:
 		stats[JAILHOUSE_CPU_STAT_VMEXITS_SMCCC]++;
@@ -99,11 +105,10 @@ enum trap_return handle_smc(struct trap_context *ctx)
 		break;
 
 	default:
-		return TRAP_UNHANDLED;
-
+		ret = TRAP_UNHANDLED;
 	}
 
 	arch_skip_instruction(ctx);
 
-	return TRAP_HANDLED;
+	return ret;
 }

commit 75c1390768a0195a6c6218dc6334fdd81e3210c8

arm-common: smccc: fix missing break statement
Without breaking the ARM_SMCCC_OWNER_ARCH case, we will fall through and
end up in the ARM_SMCCC_OWNER_SIP case which is clearly a mistake.

Fix this bug by adding the break statement for the ARM_SMCCC_OWNER_ARCH
case.

Fixes: 075879bd0d48 ("arm-common: crash cell on unhandled SMC traps")
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/arm-common/smccc.c
@@ -94,6 +94,7 @@ enum trap_return handle_smc(struct trap_context *ctx)
 	case ARM_SMCCC_OWNER_ARCH:
 		stats[JAILHOUSE_CPU_STAT_VMEXITS_SMCCC]++;
 		ret = handle_arch(ctx);
+		break;
 
 	case ARM_SMCCC_OWNER_SIP:
 		stats[JAILHOUSE_CPU_STAT_VMEXITS_SMCCC]++;